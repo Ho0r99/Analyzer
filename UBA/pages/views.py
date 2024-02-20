a36b748295d6d33ed6573b72902152633f7824dc
Hoor mohammed <157906970+Ho0r99@users.noreply.github.com>
2024-02-20 01:01:46 +0000

    COMMIT

--- UBA/pages/views.py
+++ UBA/pages/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 # Create your views here.
 
+# THIS THE FUNCTION IS CREATE  THE PAGES 
 def index(request):
-    return HttpResponse('hello world')
+ return render(request,'pages/index.html')
